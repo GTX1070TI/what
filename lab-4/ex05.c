2536ea918b46237e140c1d988c187d0f900461a6
GTX1070TI <68011601@kmitl.ac.th>
2025-08-15 09:49:10 +0000

    Write your commit note here

---
+++ lab-4/ex05.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() 
+{
+    int num, i;
+    unsigned long long factorial = 1; 
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0) 
+    {
+        return 0;
+    } 
+    else 
+    {
+        for (i = 1; i <= num; i++) 
+        {
+            factorial *= i;
+        }
+        printf("Factorial of %d is %llu\n", num, factorial);
+    }
+
+    return 0;
+}
